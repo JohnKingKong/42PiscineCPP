3b8c06f7f6b46452466defdb250408ad3525900a
Tonyg92x <anthony.guay112@gmail.com>
2022-07-04 11:51:35 -0400

    add subject and start 03-01

---
+++ CPP03/ex01/includes/classScavTrap.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   classScavTrap.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aguay <aguay@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/04 11:21:09 by aguay             #+#    #+#             */
+/*   Updated: 2022/07/04 11:40:10 by aguay            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLASSSCAVTRAP_HPP
+# define CLASSSCAVTRAP_HPP
+
+#include "classClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+	
+};
+
+#endif
