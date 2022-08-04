278cf333a84cb3ecd9626d755293908f4f26a81e
Tonyg92x <anthony.guay112@gmail.com>
2022-08-04 10:37:10 -0400

    ex00 done

--- CPP05/ex00/srcs/main/main.cpp
+++ CPP05/ex00/srcs/main/main.cpp
@@ -3,17 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anthony <anthony@student.42.fr>            +#+  +:+       +#+        */
+/*   By: aguay <aguay@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 13:20:00 by aguay             #+#    #+#             */
-/*   Updated: 2022/06/14 08:10:50 by anthony          ###   ########.fr       */
+/*   Updated: 2022/08/04 10:35:44 by aguay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "main.hpp"
+#include "classBureaucrat.hpp"
 
 int	main(void)
 {
-	printf("Hello World!\n");
+	try
+	{
+		Bureaucrat	Bob;
+		std::cout << Bob << std::endl;
+	}
+	catch (std::exception & e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+	try
+	{
+		Bureaucrat	Bob("Bob", 12);
+		Bureaucrat	Elise = Bob;
+		std::cout << Bob << std::endl;
+		Elise.decremGrade();
+		Bob.incremGrade();
+		std::cout << Elise << std::endl;
+		std::cout << Bob << std::endl;
+	}
+	catch (std::exception & e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+	}
+	
 	return (0);
 }
