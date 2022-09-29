1743d72b1d8e60cf3e692db31addc039d7a28eb4
Tonyg92x <anthony.guay112@gmail.com>
2022-09-29 07:28:59 -0400

    update 05

--- CPP05/ex00/srcs/main/main.cpp
+++ CPP05/ex00/srcs/main/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aguay <aguay@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 13:20:00 by aguay             #+#    #+#             */
-/*   Updated: 2022/08/05 07:38:37 by aguay            ###   ########.fr       */
+/*   Updated: 2022/09/28 16:34:15 by aguay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,49 @@
 
 int	main(void)
 {
-	Bureaucrat	B0;
-	Bureaucrat	B1("Clem", 12);
-	Bureaucrat	B2("BOUAHAHAHAHHA", -1);
-	Bureaucrat	B3("R2D2", 300);
-	Bureaucrat	B4 = B1;
+	//	Trying to create a default constructor (ask user input)
+	try
+	{
+		Bureaucrat	B0;
+		std::cout << B0.getName() << " created!\n" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Error :" << e.what() << '\n';
+	}
+
+	//	Try to create a Bureaucrate with valid info
+	try
+	{
+		Bureaucrat	B1("Clem", 12);
+		std::cout << B1.getName() << " created!\n" << std::endl;
+		Bureaucrat	B4 = B1;
+		std::cout << B4.getName() << " created!\n" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Error :" << e.what() << '\n';
+	}
+	
+	//	Trying ot create a Bureaucrate with invalid ID
+	try
+	{
+		Bureaucrat	B2("BOUAHAHAHAHHA", -1);
+		std::cout << B2.getName() << " created!\n" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Error :" << e.what() << '\n';
+	}
+	
+	try
+	{
+		Bureaucrat	B3("R2D2", 300);
+		std::cout << B3.getName() << " created!\n" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << "Error :" << e.what() << '\n';
+	}
 	return (0);
 }
--- CPP05/ex01/srcs/main/main.cpp
+++ CPP05/ex01/srcs/main/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aguay <aguay@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 13:20:00 by aguay             #+#    #+#             */
-/*   Updated: 2022/08/05 08:09:45 by aguay            ###   ########.fr       */
+/*   Updated: 2022/09/28 16:35:48 by aguay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,31 @@
 
 int	main(void)
 {
-	Bureaucrat	B0;
-	Form		f("Covid vaccination attestation", 92, 1);
-
-	B0.signForm(f);
-	std::cout << B0 << " " << f << std::endl;
-	Bureaucrat B1 = B0;
-	B1.decremGrade();
-	std::cout << B1 << std::endl;
-	while (B1.getGrade() > 1)
+	try
 	{
+		Bureaucrat	B0;
+		Form		f("Covid vaccination attestation", 92, 1);
+
+		B0.signForm(f);
+		std::cout << B0 << " " << f << std::endl;
+		Bureaucrat B1 = B0;
 		B1.decremGrade();
 		std::cout << B1 << std::endl;
+		while (B1.getGrade() > 1)
+		{
+			B1.decremGrade();
+			std::cout << B1 << std::endl;
+		}
+		B1.decremGrade();
+		std::cout << B1 << std::endl;
+		B1.signForm(f);
+		std::cout << f << std::endl;
 	}
-	B1.decremGrade();
-	std::cout << B1 << std::endl;
-	B1.signForm(f);
-	std::cout << f << std::endl;
+	catch(const std::exception& e)
+	{
+		std::cerr << "Error :" << e.what() << '\n';
+	}
+	
+
 	return (0);
 }
